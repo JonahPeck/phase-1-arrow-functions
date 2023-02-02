d4951b092e087fa48a2d030e4e402642d231575d
JonahPeck <PeckJonahC@gmail.com>
2023-02-01 20:43:52 -0700

    completed lab

---
+++ index.js
@@ -0,0 +1,11 @@
+const num1 = 2000;
+const num2 = 100;
+
+function divide (num1, num2) {
+    let divide = num1/num2;
+    return divide;
+}
+
+const square = (num1) => num1*num1;
+
+const add = (num1, num2) => num1+num2;
